7c29ceb25a0266ee739cfeab29af74cacb7a7bd6
江宜欣 <eason@Eason.local>
2024-03-26 19:12:09 -0400

    task 1 accomplished

--- Task1.py
+++ Task1.py
@@ -1,10 +1,13 @@
 # This is for INFSCI 2440 in Spring 2024
 # Task 1: Regression task 
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 import numpy as np
@@ -21,15 +24,17 @@ class Task1:
         self.test_data = DatasetParser(Task1.TEST_SET_PATH).data        
         
     
-    def train(self, X, y):
+    def train(self, X, y, n=27):
         # Identifying categorical columns to be one-hot encoded
         categorical_features = X.select_dtypes(include=['object']).columns
         numerical_features = X.select_dtypes(exclude=['object']).columns
 
+        base_model = RandomForestRegressor(n_estimators=40, random_state=42)
+        rfe = RFE(estimator=base_model, n_features_to_select=n)
         # Creating the preprocessing pipeline
         preprocessor = ColumnTransformer(
             transformers=[
-                ('num', MinMaxScaler(feature_range=(-100, 100)), numerical_features),
+                ('num', MinMaxScaler(feature_range=(-1, 1)), numerical_features),
                 ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
             ]
         )
@@ -37,11 +42,19 @@ class Task1:
         # Creating the modeling pipeline
         pipeline = Pipeline(steps=[
             ('preprocessor', preprocessor),
-            ('model', LinearRegression())
+            ('rfe', rfe),
+            ('model', base_model)
         ])
         pipeline.fit(X, y)
 
-        
+        selected_features_mask = pipeline.named_steps['rfe'].support_
+        preprocessed_X = pipeline.named_steps['preprocessor'].transform(X)
+        all_feature_names = pipeline.named_steps['preprocessor'].get_feature_names_out()
+        selected_features = all_feature_names[selected_features_mask]
+        print(selected_features)
+        # selected_features = X.columns[selected_features_mask]
+        # print("selected features:", selected_features)
+        # print("ranking of features:", selected_features_ranking)
         return pipeline
         
 
@@ -50,15 +63,14 @@ class Task1:
         # Train the model 1 with your best hyper parameters (if have) and features on training data.
         X = self.train_data.drop('G3', axis=1)
         y = self.train_data['G3']
-        pipeline = self.train(X, y)
-        scores = cross_val_score(pipeline, X, y, cv=10, scoring='neg_root_mean_squared_error')
-        mse_scores = -scores
-        # Computing the average RMSE across all folds
-        average_mse = np.mean(mse_scores)
-        print(average_mse)
-
-        # Evaluate learned model on testing data, and print the results.
-        print("Mean squared error\t" + str(average_mse))
+        for i in range(23, 24):
+            pipeline = self.train(X, y, i)
+            scores = cross_val_score(pipeline, X, y, cv=10, scoring='neg_mean_squared_error')
+            mse_scores = -scores
+            # Computing the average RMSE across all folds
+            average_mse = np.mean(mse_scores)
+            # Evaluate learned model on testing data, and print the results.
+            print(f"{i = }\tMean squared error\t" + str(average_mse))
         return
 
     def model_2_run(self):
@@ -66,15 +78,17 @@ class Task1:
         # Train the model 2 with your best hyper parameters (if have) and features on training data.
         X = self.train_data.drop('G3', axis=1)
         y = self.train_data['G3']
-        pipeline = self.train(X, y)
-        X_test_actual = self.test_data.drop('G3', axis=1)
-        y_test_actual = self.test_data['G3']
+        for i in range(23, 24):
 
-        y_test_pred = pipeline.predict(X_test_actual)
-        mse = mean_squared_error(y_test_actual, y_test_pred)
+            pipeline = self.train(X, y, i)
+            X_test_actual = self.test_data.drop('G3', axis=1)
+            y_test_actual = self.test_data['G3']
 
-        # Evaluate learned model on testing data, and print the results.
-        print("Mean squared error\t" + str(mse))
+            y_test_pred = pipeline.predict(X_test_actual)
+            mse = mean_squared_error(y_test_actual, y_test_pred)
+
+            # Evaluate learned model on testing data, and print the results.
+            print(f"{i = }\tMean squared error\t" + str(mse))
         return
 
 
